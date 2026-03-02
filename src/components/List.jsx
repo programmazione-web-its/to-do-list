710ef2afe831072eaaf1ce4c9c250b352e1b1005
analayze <annalisa@antonioli.dev>
2026-03-02 12:54:03 +0100

    Add contatori

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,26 +2,40 @@ import pencilIcon from '../assets/pencil.svg'
 import trashIcon from '../assets/trash.svg'
 
 function List({ listElements }) {
+  let completed = 0
+  let pending = 10
   function handleClick(pulsante) {
     console.log('Click sul pulsante ', pulsante)
   }
 
   return (
-    <ul className='list'>
-      {listElements.map((element, i) => (
-        <li className='list-item ' key={i}>
-          {element}
-          <div className='actions'>
-            <div onClick={() => handleClick(element)} className='icon-btn'>
-              <img src={pencilIcon} alt='edit' />
+    <>
+      <div className='completed-tasks'>
+        <div>
+          <span>XX</span>
+          completate
+        </div>
+        <div>
+          <span>YY</span>
+          da completare
+        </div>
+      </div>
+      <ul className='list'>
+        {listElements.map((element, i) => (
+          <li className='list-item ' key={i}>
+            {element}
+            <div className='actions'>
+              <div onClick={() => handleClick(element)} className='icon-btn'>
+                <img src={pencilIcon} alt='edit' />
+              </div>
+              <div className='icon-btn'>
+                <img src={trashIcon} alt='delete' />
+              </div>
             </div>
-            <div className='icon-btn'>
-              <img src={trashIcon} alt='delete' />
-            </div>
-          </div>
-        </li>
-      ))}
-    </ul>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
 
